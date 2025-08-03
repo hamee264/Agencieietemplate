76ed04e5c280f7585d5a7e62ce39e1270ea56b4a
BeanDev <hameedrauf978@gmail.com>
2025-08-03 22:58:06 +0100

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -58,7 +58,7 @@ header {
   padding: 20px 30px;
   z-index: 999;
   position: relative;
-  position: fixed;
+  position: static;
   width: 100%;
   z-index: 99999999;
   box-shadow: 8px 8px 16px #d1d9e6, -8px -8px 16px #ffffff;
